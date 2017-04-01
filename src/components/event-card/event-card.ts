7b69f0b8fea7abbb46bd20ee34cdcccc9d0e86bb
sdobon <sebastiandobon2@gmail.com>
2017-04-01 16:17:24 -0500

    added date/time

--- src/components/event-card/event-card.ts
+++ src/components/event-card/event-card.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Event } from '../../models/event'
 
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'event-card',
@@ -9,4 +11,25 @@ import { Event } from '../../models/event'
 
 export class EventCard {
   @Input() event: Event;
+  day: string;
+  month: string;
+  time: string;
+
+  // constructor(){
+  //   var t = moment.unix(this.event.date);
+  //   this.day = t.format("DD");
+  //   this.month = t.format("MM");
+  //   this.time = t.format("h:mm a");
+  // }
+
+
+  getDay() {
+    return moment.unix(this.event.date).format("DD");
+  }
+  getMonth() {
+    return moment.unix(this.event.date).format("MMM");
+  }
+  getTime(){
+    return moment.unix(this.event.date).format("h:mm a");
+  }
 }
